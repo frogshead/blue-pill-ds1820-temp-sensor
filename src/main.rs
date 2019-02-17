0c902645534acf051912c41e558f757bb8f6c61d
Mikko Viitamäki <mikko.vitamaki@gmail.com>
2019-02-17 13:45:22 +0200

    blinky compiles  and openocd is able to connect.

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,56 @@
-#![no_main]
+//! Blinks an LED
+
+#![deny(unsafe_code)]
+#![deny(warnings)]
 #![no_std]
+#![no_main]
 
-// set the panic handler
+extern crate cortex_m;
+extern crate cortex_m_rt as rt;
 extern crate panic_semihosting;
+extern crate stm32f1xx_hal as hal;
+#[macro_use(block)]
+extern crate nb;
 
-use core::sync::atomic::{AtomicBool, Ordering};
-use cortex_m::peripheral::syst::SystClkSource;
-use cortex_m_rt::{entry, exception};
 use hal::prelude::*;
-
-static TOGGLE_LED: AtomicBool = AtomicBool::new(false);
+use hal::stm32;
+use hal::timer::Timer;
+use rt::{entry, exception, ExceptionFrame};
 
 #[entry]
 fn main() -> ! {
-    let mut core = cortex_m::Peripherals::take().unwrap();
-    let device = hal::stm32f103xx::Peripherals::take().unwrap();
-    let mut rcc = device.RCC.constrain();
-    let mut flash = device.FLASH.constrain();
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = stm32::Peripherals::take().unwrap();
 
-    let clocks = rcc
-        .cfgr
-        .use_hse(8.mhz())
-        .sysclk(16.mhz())
-        .freeze(&mut flash.acr);
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
 
-    // configure the user led
-    let mut gpioc = device.GPIOC.split(&mut rcc.apb2);
+    // Try a different clock configuration
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    // let clocks = rcc.cfgr
+    //     .sysclk(64.mhz())
+    //     .pclk1(32.mhz())
+    //     .freeze(&mut flash.acr);
+
+    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-
-    // configure SysTick to generate an exception every second
-    core.SYST.set_clock_source(SystClkSource::Core);
-    core.SYST.set_reload(clocks.sysclk().0);
-    core.SYST.enable_counter();
-    core.SYST.enable_interrupt();
-
+    // Try a different timer (even SYST)
+    let mut timer = Timer::syst(cp.SYST, 1.hz(), clocks);
     loop {
-        // sleep
-        cortex_m::asm::wfi();
-        if TOGGLE_LED.swap(false, Ordering::AcqRel) {
-            led.toggle();
-        }
+        block!(timer.wait()).unwrap();
+        led.set_high();
+        block!(timer.wait()).unwrap();
+        led.set_low();
     }
 }
 
 #[exception]
-fn SysTick() {
-    TOGGLE_LED.store(true, Ordering::Release);
+fn HardFault(ef: &ExceptionFrame) -> ! {
+    panic!("{:#?}", ef);
+}
+
+#[exception]
+fn DefaultHandler(irqn: i16) {
+    panic!("Unhandled exception (IRQn = {})", irqn);
 }
