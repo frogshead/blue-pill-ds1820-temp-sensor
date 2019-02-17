de0d3aeb9b83681ed2e3f9b7deb4c9da0dd2cd66
Mikko Viitamäki <mikko.vitamaki@gmail.com>
2019-02-18 00:00:09 +0200

    get measurements from ds18b20

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,25 @@ extern crate cortex_m;
 extern crate cortex_m_rt as rt;
 extern crate panic_semihosting;
 extern crate stm32f1xx_hal as hal;
-#[macro_use(block)]
-extern crate nb;
+extern crate onewire;
+
+// #[macro_use(block)]
+// extern crate nb;
 
 use hal::prelude::*;
+use hal::delay::Delay;
 use hal::stm32;
-use hal::timer::Timer;
+//use hal::timer::Timer;
 use rt::{entry, exception, ExceptionFrame};
+
+extern crate cortex_m_semihosting;
+
+use cortex_m_semihosting::hio;
+use core::fmt::Write;
+
+use onewire::DS18B20;
+use onewire::ds18b20;
+
 
 #[entry]
 fn main() -> ! {
@@ -24,6 +36,8 @@ fn main() -> ! {
 
     let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
+    let mut stdout = hio::hstdout().unwrap();
+    write!(stdout, "Rust on embedded is 1!\n").unwrap();
 
     // Try a different clock configuration
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
@@ -33,15 +47,49 @@ fn main() -> ! {
     //     .freeze(&mut flash.acr);
 
     let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+    
+
+
+    let mut onewire_pin = gpiob.pb12.into_open_drain_output(&mut gpiob.crh).downgrade();
+    let mut delay = Delay::new(cp.SYST, clocks);
+    
+    let mut _onewire = onewire::OneWire::new(&mut onewire_pin, false);
+    let mut search = onewire::DeviceSearch::new();
+    while let Some(device) = _onewire.search_next(&mut search ,&mut delay).unwrap(){
+        match device.address[0]{
+            ds18b20::FAMILY_CODE => {
+                let ds18b20 = DS18B20::new(device).unwrap();
+                let resolution = ds18b20.measure_temperature(&mut _onewire, &mut delay).unwrap();
+                delay.delay_ms(resolution.time_ms());
+                loop{
+                  
+                    ds18b20.measure_temperature(&mut _onewire, &mut delay).unwrap();
+                    write!(stdout, "Temperature: {}\n", ds18b20.read_temperature(&mut _onewire, &mut delay).unwrap()).unwrap();
+                }    
+            },
+            _ => {
+                write!(stdout, "Can't read temperature!").unwrap();
+
+            }
+
+
+            }
+        }
+
+    
 
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
+
+    //let mut timer = Timer::syst(cp.SYST, 10.hz(), clocks);
     // Try a different timer (even SYST)
-    let mut timer = Timer::syst(cp.SYST, 1.hz(), clocks);
     loop {
-        block!(timer.wait()).unwrap();
+        // block!(timer.wait()).unwrap();
+        // led.set_high();
+        // block!(timer.wait()).unwrap();
+        // led.set_low();
         led.set_high();
-        block!(timer.wait()).unwrap();
-        led.set_low();
     }
 }
 
